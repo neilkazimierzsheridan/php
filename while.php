24ddcefc278f17a97d3a19e039f22187cc1cf695
nks sheridan <nk.sheridan@zoho.com>
2024-10-18 15:17:26 +0100

    php while

---
+++ while.php
@@ -0,0 +1,36 @@
+<html>
+<head><title>PHP Test</title></head>
+	<body>
+		
+		
+
+		<?php
+
+			echo "THE WHILE LOOP <br>";
+			$index = 1;
+			while($index <=30){
+				echo " index= $index <br>";
+				$index++;
+			}
+
+			echo "<br>";
+			echo "THE DO WHILE LOOP <br>";
+
+			$index = 1;
+			do{
+				echo "index = $index <br>";
+				$index++;
+			}while($index <=6);
+
+		?>
+
+
+
+
+
+
+
+
+	</body>
+
+</html>
