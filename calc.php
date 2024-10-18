7ef36b3cfcadf663769bcfa8ea9101eafeba1b01
nks sheridan <nk.sheridan@zoho.com>
2024-10-18 13:43:23 +0100

    PHP calc

--- calc.php
+++ calc.php
@@ -2,8 +2,8 @@
 <head><title>PHP Test</title></head>
 	<body>
 		<form actions="test.php" method="post">
-			First Number: <input type="number" name="num1"> <br>
-			Second Number: <input type="number" name="num2"> <br>
+			First Number: <input type="number" step="0.1" name="num1"> <br>
+			Second Number: <input type="number" step="0.1" name="num2"> <br>
 			Operator: <input type="text" name="op"> <br>
 			<input type="submit">
 
@@ -21,7 +21,9 @@
 			} elseif($op == "/"){
 				echo $num1 / $num2;
 			} elseif($op == "*"){
-				echo $num1 * $num2
+				echo $num1 * $num2;
+			} else {
+				echo "Invalid Operation";
 			}
 
 		?>
