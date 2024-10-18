376439ff49cce84ccd2deff314058d482fecb6af
nks sheridan <nk.sheridan@zoho.com>
2024-10-18 15:51:28 +0100

    php includes

---
+++ site.php
@@ -0,0 +1,29 @@
+<html>
+<head><title>PHP Test</title></head>
+	<body>
+		
+		
+
+		<?php
+			$title = "My Blog Post";
+			$author = "me";
+			$wordCount = "323";
+			include "article-header.php";
+			echo "<br> FROM THE USER-TOOLS FILE <br>";	
+			include "user-tools.php";
+			sayHi("Sarah");
+			echo"<br>";
+			echo $feetInMile;
+
+		?>
+
+
+
+
+
+
+
+
+	</body>
+
+</html>
