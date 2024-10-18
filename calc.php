ee90182544e79ff5740e911d8577787c12a1563d
nks sheridan <nk.sheridan@zoho.com>
2024-10-18 13:30:17 +0100

    PHP calc

---
+++ calc.php
@@ -0,0 +1,38 @@
+<html>
+<head><title>PHP Test</title></head>
+	<body>
+		<form actions="test.php" method="post">
+			First Number: <input type="number" name="num1"> <br>
+			Second Number: <input type="number" name="num2"> <br>
+			Operator: <input type="text" name="op"> <br>
+			<input type="submit">
+
+		</form>
+
+		<?php
+			$num1 = $_POST["num1"];
+			$num2 = $_POST["num2"];
+			$op = $_POST["op"];
+
+			if($op == "+"){
+				echo $num1 + $num2;
+			} elseif($op == "-"){
+				echo $num1 - $num2;
+			} elseif($op == "/"){
+				echo $num1 / $num2;
+			} elseif($op == "*"){
+				echo $num1 * $num2
+			}
+
+		?>
+
+
+
+
+
+
+
+
+	</body>
+
+</html>
